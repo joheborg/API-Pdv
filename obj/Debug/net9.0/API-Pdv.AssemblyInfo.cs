bc4b160f686d2e74ea928d79a62e1e5461c95c4c
Jonathan Ferreira <40966221+joheborg@users.noreply.github.com>
2025-06-10 20:09:23 -0300

    ++

--- obj/Debug/net9.0/API-Pdv.AssemblyInfo.cs
+++ obj/Debug/net9.0/API-Pdv.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API-Pdv")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e2baef7427b824a17574da27afd95cc17f76037")]
 [assembly: System.Reflection.AssemblyProductAttribute("API-Pdv")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API-Pdv")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
